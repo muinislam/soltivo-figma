5c1c28de480679a237b34510a137091252bfd340
unknown <mohebbullah4045@gmail.com>
2023-06-13 21:21:08 +0600

    first commit

---
+++ js/sript.js
@@ -0,0 +1,17 @@
+ $('.banner_wrapper').slick({
+   infinite:true,
+   prevArrow:'.prev',
+    nextArrow:'.next',
+    slidesToShow:1,
+    slidesToScroll:1
+   
+ });
+ $('.proces_slide').slick({
+   prevArrow:'.prev_icon',
+    nextArrow:'.next_icon',
+    slidesToShow:3,
+    slidesToScroll:1,
+    infinite:true
+   
+ });
+ 
